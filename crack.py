07477e953c261b765d9a1657c101083581d7756f
Emre E <121287269+diabloakar0@users.noreply.github.com>
2023-06-08 19:44:56 +0200

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,8 +1,11 @@
-import rarfile
+import subprocess
 import string
 import itertools
 import os
 from colorama import Fore
+import rarfile
+
+rarfile.UNRAR_TOOL = r"C:\Program Files\WinRAR\UnRAR.exe"
 
 doblogibidoblo = '''
 ⠀⠀⢀⣴⣶⣿⣿⣷⡶⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⢶⣿⣿⣿⣿⣶⣄⠀⠀
@@ -23,6 +26,10 @@ doblogibidoblo = '''
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+'''
+alt = '''
+
+
 Bu maskenin altında etten fazlası var. 
 Bu maskenin altında bir fikir var, Bay Creedy. 
 Ve fikirlere, kurşun işlemez.
@@ -31,28 +38,35 @@ coded by. diablo
 Instagram: diabloakar82
 Twitter: diabloakar
 Discord: diablo akar#1338
+
+
 '''
 
 os.system("cls")
 print(Fore.GREEN + doblogibidoblo)
-rar_file = input(Fore.RED + "RAR dosya adını girin: "+Fore.WHITE)
-found = False
+print(alt)
 
 
 def crack_rar(password):
-    rarfile.UNRAR_TOOL = r"C:\Program Files\WinRAR\UnRAR.exe"  # unar aracının doğru yoluyla güncelleyin
+    global found
     try:
-        with rarfile.RarFile(rar_file) as rf:
-            rf.extractall(pwd=password)
-            print("Şifre çözüldü:", password)
-            found = True
-            with open("şifre.txt", "w") as f:
-                f.write(password)
-                print("Şifre şifre.txt dosyasına kaydedildi.")
-    except rarfile.BadRarFile:
-        pass
-    except rarfile.BadRarPassword:
-        pass
+        cmd = f'unrar x -p"{password}" -inul "{rar_file}"'
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+        print("Şifre çözüldü:", password)
+        found = True
+        with open("şifre.txt", "w") as f:
+            f.write(password)
+            print("Şifre şifre.txt dosyasına kaydedildi.")
+    except subprocess.CalledProcessError as e:
+        if "CRC failed" in e.output.decode('utf-8', 'ignore'):
+            pass
+        elif "No files to extract" in e.output.decode('utf-8', 'ignore'):
+            pass
+        elif "Wrong password?" in e.output.decode('utf-8', 'ignore'):
+            pass
+        else:
+            print("Hata: RAR aracı çalıştırılamadı.")
+            print(e.output.decode('utf-8', 'ignore'))
 
 
 def character_crack():
@@ -63,6 +77,8 @@ def character_crack():
         for password in itertools.product(characters, repeat=password_length):
             password = "".join(password)
             crack_rar(password)
+            if found:
+                return
 
         password_length += 1
 
